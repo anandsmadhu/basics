644d14504fba5e268c31a10b88013f89448836c6
anandsmadhu <anandsmadhu@yahoo.com>
2017-10-02 07:37:26 -0400

    Stack sample code

--- ds/stack.c
+++ ds/stack.c
@@ -1,31 +1,67 @@
 # include "stdio.h"
+# include "stdlib.h"
 
 typedef struct stack
 {
 	int data;
 	struct stack *next;
-} STACK;
+} STACKITEM;
 
+void stackPush(STACKITEM **stackPtr, int data)
+{
+	STACKITEM *temp = NULL;
+	if(*stackPtr ==NULL)
+	{
+		printf("stack is empty\n");
+		*stackPtr=(STACKITEM *) malloc(sizeof(STACKITEM));
+		(*stackPtr)-> data = data;
+		(*stackPtr)-> next = NULL;
+	}
+	else
+	{
+		printf("stack is not empty\n");
+		temp = (STACKITEM *) malloc(sizeof(STACKITEM));
+		temp->data = data;
+		temp->next = *stackPtr;
+		*stackPtr = temp;
+	}
+}
 
-
-void stackInit(STACK **stackPtr)
+void stackPop(STACKITEM **stackPtr)
 {
 }
 
-
-void stackPush(STACK **stackPtr)
+void printStack(STACKITEM *stackPtr)
 {
+	if(stackPtr!=NULL)
+	{
+		while(stackPtr != NULL)
+		{
+			printf("data = %d\n", stackPtr->data);
+			stackPtr = stackPtr->next;
+		}
+
+	}
+	else
+	{
+		printf("stack is empty\n");
+	}
 }
 
-void stackPop(STACK **stackPtr)
-{
-}
 
 
 
 int main(void)
 {
-	STACK *stackPtr;
+	STACKITEM *stackPtr=NULL;
 	
+	stackPush(&stackPtr,2);
+	stackPush(&stackPtr,4);
+	stackPush(&stackPtr,6);
+	stackPush(&stackPtr,8);
+	stackPush(&stackPtr,10);
+	stackPush(&stackPtr,12);
+	stackPush(&stackPtr,14);
+	printStack(stackPtr);
 	return(0);
 }
