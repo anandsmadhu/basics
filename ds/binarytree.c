3b033cfd258a63b1105dcf24a12d1660f0401247
anandsmadhu <anandsmadhu@yahoo.com>
2017-12-10 17:42:32 -0500

    Added files for dlist. started binary tree

--- ds/binarytree.c
+++ ds/binarytree.c
@@ -24,41 +24,23 @@ Breadth First or Level Order Traversal : 1 2 3 4 5
 
 static int level = 0;
 
-struct btreenode
+typedef struct btreenode
 {
 	int data;
 	struct btreenode *left;
 	struct btreenode *right;
-};
+} TREENODE;
 
-
-void insertNode(struct btreenode **root, int data)
-{
-	if((*root) == NULL)
-	{
-		*root = (struct btreenode *)malloc(sizeof (struct btreenode));
-		(*root)->left = NULL;
-		(*root)->right = NULL;
-		(*root)->data = data;
-	}
-	else
-	{
-		if(data < ((*root)->data))
-			insertNode (&((*root)->left), data);
-		else
-			insertNode (&((*root)->right), data);
-	}
-}
 
 int main(void)
 {
-	struct btreenode *root;
-	root = NULL;
-	insertNode(&root, 10);
+	struct TREENODE *root = NULL;
+
+/*	insertNode(&root, 10);
 	insertNode(&root, 07);
 	insertNode(&root, 19);
 	insertNode(&root, 84);
 	insertNode(&root, 23);
-	insertNode(&root, 11);
+	insertNode(&root, 11);*/
 }
 
---
+++ ds/dlist.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "pthread.h"
+#include "semaphore.h"
+
+struct node
+{
+	int num;
+	struct node *prev;
+	struct node *next;
+}
+
+
+
+
