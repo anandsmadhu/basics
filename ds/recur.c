ba759b5698034c66876fe8854cbc8044807634cb
anandsmadhu <anandsmadhu@yahoo.com>
2018-04-22 11:24:43 -0400

    working out recusrion

--- ds/recur.c
+++ ds/recur.c
@@ -2,7 +2,60 @@
 #include "stdlib.h"
 
 
+
+void printbits(int n)
+{
+    while(n!=0)
+    {
+        printf("%d ", n&0x1);
+        n=n>>1;
+    }
+}
+
+void recprintbits(int n)
+{
+    if(n!=1)
+        recprintbits(n>>1);
+    
+    printf("%d ", n&0x1);
+}
+
+void revbits(int n)
+{
+    
+    
+
+}
+
+
+
+int factorial (int n)
+{
+    int fact;
+    if (n==0)
+        fact=1;
+    else
+        fact=n*factorial(n-1);
+    
+    return(fact);
+}
+
+
+void printnum(int n)
+{
+    if(n!=1)
+        printnum(n-1);
+    printf("%d, ", n);
+
+}
+
 int main (void)
 {
+    
+    //printnum(10);
+    //printf("factorial of 5=%d\n" ,factorial(5));
+    printbits(8);
+    printf("\n");
+    recprintbits(8);
     return (0);
 }
