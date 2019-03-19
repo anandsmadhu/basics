086a8c700b390da78cc9868e8f67dae5817813df
U-LENOVO\amadhusoodan <amadhusoodan@AMADHUSOODA-FVP.lenovo.com>
2019-03-19 13:29:41 -0400

    Added workspace files

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -11,12 +11,55 @@ int main(void)
     vector<int> intvector; // simple vector declaration
     vector<int> ivec (10, 1);
 
+    string word;
+    vector<string> inputtext;
+    int count = 0;
+
+    
+
     //Accessing vector using subscript
-    for(int i=0; i<10;i++)
+    for(int i=0; i<ivec.size();i++)
     {
         cout << ivec[i] << endl;
     }
+
+    cout <<"After pushback" << endl;
+
+    ivec.push_back(2);
+    for(int i=0; i<ivec.size();i++)
+    {
+        cout << ivec[i] << endl;
+    }    
     //Accessing vector using subscript
+
+    cout << "Size of vector is : " << ivec.size() <<endl;
+
+    while(cin >> word && count <=3)
+    {
+        inputtext.push_back(word);
+        count++;
+    }
+
+    // iterators
+    //string::iterator si;
+    cout << "Testng iterators" <<endl;
+    auto sib = inputtext.begin();
+    auto size = inputtext.size();
+
+    while(sib <= size)
+    {
+        cout << *sib << endl;
+        sib++;
+
+    }
+
+    //for(si = inputtext.begin(); si!=inputtext.end(); si++)
+       // cout<< *si << endl;
+    
+
+
+
+    // iterators
 
     return(0);
 
