8ac514f38f2103150af0076b60f2eb64a756493f
U-LENOVO\amadhusoodan <amadhusoodan@AMADHUSOODA-FVP.lenovo.com>
2019-04-23 12:08:44 -0400

    backtracking examples

--- backtracking/dierolls.cpp
+++ backtracking/dierolls.cpp
@@ -5,19 +5,56 @@ what is passed on to further calls
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+void roll(int dice, vector <int> &choice)
+{
+    if(dice == 0)
+    {
+        cout<<"{";
+        for(int idx = 0; idx<choice.size(); idx++)
+        {
+            cout<<choice[idx]<<",";
+        }
+        cout<<"}"<<endl;
+    }
+    else
+    {
+        for(int diechoice=1; diechoice<=6;diechoice++)
+        {
+            //choose for die 1
+            choice.push_back(diechoice);
+
+            //explore
+            roll(dice-1, choice);
+
+            // undo choice
+
+            choice.pop_back();
+
+
+
+        }
+    }
+    
+}
 
 
 void rolldice(int dice)
 {
+    if(dice ==0)
+        return;
+    vector <int> choice;
+    roll(dice, choice);
     
 }
 
 
 int main(void)
 {
-    int dice = 5;
+    int dice = 2;
 
-    rolldice(5);
+    rolldice(dice);
 }
